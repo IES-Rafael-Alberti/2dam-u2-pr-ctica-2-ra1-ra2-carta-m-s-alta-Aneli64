551d132b48e35914cc1ceb189b3715d223027b9e
Aneli64 <eliandeniz27@gmail.com>
2023-11-13 11:55:07 +0100

    avances clase Baraja

---
+++ app/src/main/java/com/example/cartaalta/Baraja.kt
@@ -0,0 +1,36 @@
+package com.example.cartaalta
+
+enum class Palos {
+    CORAZONES, DIAMANTES, TREBOLES, PICAS
+}
+
+enum class Naipes {
+    AS, DOS, TRES, CUATRO, CINCO, SEIS, SIETE, OCHO, NUEVE, DIEZ, REINA, REY
+
+} //VALET, DAME, ROI
+class Baraja {
+    //meteremos todos los metodos dentro de nuestro companion object,
+    // ya que lo llamaremos estaticamente
+    companion object{
+     var listaCartas = arrayListOf<Carta>()
+        //Generar la lista de 52 cartas de la baraja.
+        fun crearBaraja(){
+            var idCarta = 1
+            for (palo in Palos.values()) {
+                for (numero in Naipes.values()) {
+                    val newCarta = Carta(numero, palo, numero.ordinal + 1, numero.ordinal + 1, idCarta)
+                    if (numero.name == "AS") newCarta.puntosMax = 11
+                    listaCartas.add(newCarta)
+                    idCarta++
+                }
+            }
+        }
+
+        //Desordenar las cartas de la lista de cartas de la baraja (shuffle)
+        fun barajar() = listaCartas.shuffle()
+
+        //Retorna la última carta de la lista de cartas y la elimina de la baraja.
+        fun dameCarta() = listaCartas.removeLast()
+
+    }
+}
---
+++ app/src/main/java/com/example/cartaalta/Carta.kt
@@ -0,0 +1,8 @@
+package com.example.cartaalta
+class Carta(var nombre: Naipes, var palo: Palos, var puntosMin: Int = 0,
+            var puntosMax: Int = 0, var idDrawable: Int = 0) {
+    override fun toString(): String {
+        return "Carta(nombre=$nombre, palo=$palo, puntosMin=$puntosMin, puntosMax=$puntosMax, idDrawable=$idDrawable)"
+    }
+
+}
