1c1698c587222d867dbd5e61ce66e3305622f2cd
Aneli64 <eliandeniz27@gmail.com>
2023-11-14 16:20:55 +0100

    ultimos retoques

--- app/src/main/java/com/example/cartaalta/Carta.kt
+++ app/src/main/java/com/example/cartaalta/Carta.kt
@@ -1,4 +1,13 @@
 package com.example.cartaalta
+
+/**
+ * Clase donde se almacena nuestra carta
+ * @param nombre Numero del naipe
+ * @param palo Figura a la que hace referencia
+ * @param puntosMin Puntuacion minima que tiene (Solo cambia en caso de AS)
+ * @param puntosMax Puntuacion maxima (Solo cambia en caso de AS)
+ * @param idDrawable Id que identifica nuestra carta en la baraja
+ */
 class Carta(var nombre: Naipes, var palo: Palos, var puntosMin: Int = 0,
             var puntosMax: Int = 0, var idDrawable: Int = 0) {
     override fun toString(): String {
--- app/src/main/java/com/example/cartaalta/Pantalla.kt
+++ app/src/main/java/com/example/cartaalta/Pantalla.kt
@@ -1,10 +1,15 @@
 package com.example.cartaalta
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,14 +20,20 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 
 @Preview
 @Composable
 fun Juego() {
-
+    //Variables necesarias para nuestro juego
     val context = LocalContext.current
     var dorsoCarta by rememberSaveable { mutableStateOf("detras") }
     var idCarta by rememberSaveable {
@@ -34,6 +45,13 @@ fun Juego() {
             )
         )
     }
+    //Imagen de fondo
+    Image(
+        painter = painterResource(id = R.drawable.fondo_poker), contentDescription = "",
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(painterResource(id = idCarta))
+    )
 
     Column(
         Modifier.fillMaxSize(),
@@ -45,7 +63,7 @@ fun Juego() {
                 painter = painterResource(id = idCarta), contentDescription = ""
             )
         }
-        Row {
+        Row (Modifier.padding(20.dp)){
             Button(onClick = {
                 //Si se acaba nuestra baraja, vuelve a crear una nueva (para que no cierre el programa)
                 Baraja.barajar()
@@ -60,8 +78,8 @@ fun Juego() {
             }) {
                 Text(text = "Dame carta")
             }
-        }
-        Row {
+
+            //Boton quen os baraja nuestras cartas y pone de nuevo la carta boca abajo
             Button(onClick = {
                 Baraja.crearBaraja()
                 Baraja.barajar()
@@ -70,6 +88,7 @@ fun Juego() {
                 Text(text = "Reiniciar")
             }
         }
+        //Metodo que nos permite ir actualizando las cartas
         LaunchedEffect(dorsoCarta) {
             val id = context.resources.getIdentifier(dorsoCarta, "drawable", context.packageName)
             idCarta = id
